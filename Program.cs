b003b313c82b0fad60019d1995f2b7a8e8d02a2b
aliberkayberber <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>
2024-08-10 14:46:58 +0300

    firs commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int[] intArray = new int[10];
+
+for (int i = 0; i < intArray.Length; i++)
+{
+    intArray[i] = i;
+}
+
+foreach (var item in intArray)
+{
+    System.Console.WriteLine(item);
+}
+System.Console.Write("Lütfen 11. elemanı giriniz: ");
+
+string stringInput =Console.ReadLine();
+int intInput=0;
+
+int.TryParse(stringInput, out intInput);
+
+Array.Resize(ref intArray, intArray.Length+1);
+
+intArray[10]=intInput;
+foreach (var item in intArray)
+{
+    System.Console.WriteLine(item);
+}
+
+Array.Sort(intArray);
+Array.Reverse(intArray,0,intArray.Length);
+System.Console.WriteLine("----------------Yeni dizi-------------");
+foreach (var item in intArray)
+{
+    System.Console.WriteLine(item);
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Array Pratikleri
+## C# dilinde bir array' e aşağıdaki yöntemleri uyguladım.
+
+    ** Sort
+    ** Resize
+    ** Reverse
